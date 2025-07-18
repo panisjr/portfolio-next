e42e699b6d97fb7b598b4a616fa57f83e05e3863
rams <ramelopanisjr.06@gmail.com>
2025-07-18 22:40:54 +0800

    resolve client side issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-"use client";
+"use client"
+import React from "react";
+import Navbar from "@/components/Navbar";
+import LandingPage from "@/components/LandingPage";
+import LatestWorkPage from "@/components/LatestProjectsPage";
+import FrameworksList from "@/components/FrameworksList";
+
 export default function Home() {
   return (
-    <div className="w-full min-h-screen">
-      <main className="w-full max-w-screen">
-        <Home />
-      </main>
+    <div className="w-[700px] md:w-full h-fit">
+      <div className="mx-auto">
+        {/* Header */}
+        <Navbar />
+        {/* Main sections */}
+        <LandingPage />
+        <FrameworksList />
+        <LatestWorkPage />
+      </div>
     </div>
   );
 }
--- src/components/animate/SlideRight.tsx
+++ src/components/animate/SlideRight.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { useEffect } from "react";
