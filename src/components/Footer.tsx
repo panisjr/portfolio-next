b98086de5d737fa202cbbb03ad5a4bb10d1241df
rams <ramelopanisjr.06@gmail.com>
2025-07-18 23:33:41 +0800

    Udpate

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,18 @@ export default function Footer() {
   return (
     <footer className="w-full h-[480px] bg-[#e5e7eb]">
       {/* top-left,top-right,bottom-right, bottom-left, top-right */}
-      <div className="w-full h-full bg-[#303030] [clip-path:polygon(100%_0%,100%_0%,100%_100%,0%_100%,0%_110px)]"></div>
+      <div className="w-full h-full bg-[#303030] [clip-path:polygon(100%_0%,100%_0%,100%_100%,0%_100%,0%_110px)] pt-[170px]">
+        <div className="w-full">
+          <div className="w-full ">
+            <p className="text-white font-amarante">Ramz</p>
+          </div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </footer>
   );
 }
